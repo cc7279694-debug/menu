a34b1851a4e89f94a9acdf22e8bb88bebe97c16f
cc7279694-debug <cc7279694@gmail.com>
2026-08-28 23:24:12 +0800

    chore(recipes): log safe rpc failure details

--- src/features/recipes/queries.ts
+++ src/features/recipes/queries.ts
@@ -86,6 +86,12 @@ async function loadRecipeSearchRowsForClient(
   });
 
   if (error) {
+    console.error("[recipes] search RPC failed", {
+      code: error.code,
+      message: error.message,
+      details: error.details,
+      hint: error.hint,
+    });
     throw new Error(input.errorMessage);
   }
 
